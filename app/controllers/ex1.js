d332e976c6d1f8b3d9e4b4d0bd0646a4aea58576
Taino Corentin <cprentintaino2b@gmail.com>
2022-01-26 16:09:47 +0100

    Ajout de l action save

--- app/controllers/ex1.js
+++ app/controllers/ex1.js
@@ -7,6 +7,7 @@ export default class Ex1Controller extends Controller {
   @tracked content = 'Entrez votre texte';
   @tracked info = '';
   MAX = 100;
+  @tracked saveC = 'Entrez votre texte';
 
   get size(){
     return this.MAX - this.content.length;
@@ -15,5 +16,14 @@ export default class Ex1Controller extends Controller {
   @action
   clear(){
     this.content = '';
+  }
+
+  @action
+  save(){
+    this.saveC = this.content;
+  }
+
+  get estSave(){
+    {{if(this.content!=this.saveC) return 'Note Modifier'; return 'Note Sauvegarder'}}
   }
 }
