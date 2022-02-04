17751e187a4312ac478d8e2bf3e46bab05932a10
Taino Corentin <cprentintaino2b@gmail.com>
2022-02-04 15:47:19 +0100

    Ajout d un premier plural, affichage different en fonction du nombre de services actifs

--- app/services/product-services.js
+++ app/services/product-services.js
@@ -1,19 +1,26 @@
 import Service from '@ember/service';
+import { action } from '@ember/object';
 import { dProducts, dPromos } from 'tds/data/datas';
 
 export default class ProductServicesService extends Service {
+  products = dProducts;
+  promos = dPromos;
 
-
-  get activeServices(){
-    return dProducts.filterBy('active',true);
+  get activeServices() {
+    return this.products.filterBy('active', true);
   }
 
-  get countActive(){
-    return this.activeServices.size;
+  get countActive() {
+    return this.activeServices.length;
   }
 
-  get sumActive(){
-    const reducer = (previousValue, currentValue);
-    return this.activeServices.reduce(reducer);
+  get sumActive() {
+    //const reducer = (previousValue, currentValue) => previousValue + currentValue;
+    //var total = this.activeServices.reduce(getSum, 0);
+    return this.activeServices.reduce(0, 0);
   }
+  /*
+  function sum(total, num) {
+    return total + Math.round(num);
+  }*/
 }
